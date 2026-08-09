67a2a8ae8f0a5ef176a09fce5da8fb4e90ac1a03
nonocr <Donovanxcr@gmail.com>
2026-08-08 19:56:56 -0600

    Crear componentes principales e hijos

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+})
+
+export default class DashboardPage {}
---
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page.component',
+  imports: [],
+  templateUrl: './search-page.component.html',
+})
+export default class SearchPageComponent {}
