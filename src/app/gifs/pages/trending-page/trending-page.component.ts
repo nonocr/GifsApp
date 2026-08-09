e7a1e49fc5192fd08ed7c7e5b58599b24bd130e6
nonocr <Donovanxcr@gmail.com>
2026-08-09 00:02:32 -0600

    Mostrar imagenes de relleno

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
-  selector: 'app-trending-page.component',
-  imports: [],
+  selector: 'trending-page.component',
+  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {}
