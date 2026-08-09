4f6791ce9384a4176aa3985ff2843b10c3eb5358
nonocr <Donovanxcr@gmail.com>
2026-08-08 22:36:09 -0600

    RouterLinks desde componentes hijos

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+
+interface menuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class GifsSideMenuOptions {}
+export class GifsSideMenuOptions {
+
+  menuOptions: menuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+       subLabel: 'Gifs Populares',
+       route: '/dashboard/trending'
+    },
+
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+       subLabel: 'Buscar gifs',
+       route: '/dashboard/search'
+    },
+
+
+  ]
+}
