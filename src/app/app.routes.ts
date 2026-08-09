ff3ed187f676c9e51d55f0331a44303a61bc8dc6
nonocr <Donovanxcr@gmail.com>
2026-08-08 20:17:14 -0600

    Configurar rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,26 @@ export const routes: Routes = [
 
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component')
-  },
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+    children:
+   [
 
-  {
+    {
     path: 'trending',
     loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
-  },
+    },
 
-  {
+    {
     path: 'search',
     loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+    },
+
+    {
+    path: '**',
+    redirectTo: 'trending'
+  },
+
+   ],
   },
 
   {
